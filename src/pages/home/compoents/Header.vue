a1e719d36633ed5de365d7031bf670111305556b
genjilee <957867991@qq.com>
2019-01-28 16:58:06 +0800

    Initial commit

---
+++ src/pages/home/compoents/Header.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市景点主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style scoped lang="stylus" >
+  @import "~styles/varibles.styl"
+  //stylus指向ssrc/assets/stylus目录
+  .header
+    display: flex
+    line-height: .86rem
+    background: $bgColor
+    color: #ffffff
+    .header-left
+      width: .64rem
+      float: left
+      .back-icon
+        text-align: center
+        font-size .4rem
+    .header-input
+      flex: 1
+      height: .64rem
+      line-height: .64rem
+      margin-top: .12rem
+      margin-left:  .2rem
+      padding-left: .2rem
+      background: #ffffff
+      border-radius: .1rem
+      color: #ccc
+    .header-right
+      width: 1.24rem
+      float right
+      text-align: center
+      .arrow-icon
+        margin-left: -.04rem
+        font-size: .24rem
+
+</style>
